25d2372037285269111f16b3b9d7304cc3b6169b
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-07-21 09:51:45 +0900

    chore init log-procesor application

---
+++ application/log-processor/src/test/java/com/logmonitoring/log_processor/LogProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logmonitoring.log_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
